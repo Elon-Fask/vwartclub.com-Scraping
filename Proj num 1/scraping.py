4035623a9d1fb966e31a98d05cf84dd6a6729538
Elon-Fask <atman.boulaajaili@gmx.us>
2022-10-29 20:26:39 +0100

    update the script

--- Proj num 1/scraping.py
+++ Proj num 1/scraping.py
@@ -32,8 +32,10 @@ studio_title = driver.find_elements(By.XPATH, "/html/body/div[9]/div/div/div[1]/
 studio_country = driver.find_elements(By.CLASS_NAME, "subtitle")
 studio_city = driver.find_elements(By.CLASS_NAME, "overtitle")
 
-website_link = driver.find_elements(By.CSS_SELECTOR, "#column_left > div:nth-child(5) > div:nth-child(1) > a:nth-child(1)")
-email = driver.find_elements(By.XPATH, "/html/body/div[9]/div/div/div[1]/div[5]/div[2]/a")
+websites = driver.find_elements(By.XPATH, "/html/body/div[9]/div/div/div[1]/div[5]/div[1]/a")
+
+
+emails = driver.find_elements(By.XPATH, "/html/body/div[9]/div/div/div[1]/div[5]/div[2]/a")
 socail_media_behance = driver.find_elements(By.CSS_SELECTOR, "#column_left > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1)")
 socail_media_facebook = driver.find_elements(By.CSS_SELECTOR, "#column_left > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(2)")
 socail_media_instagram = driver.find_elements(By.CSS_SELECTOR, "#column_left > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(3)")
@@ -45,31 +47,34 @@ socail_media_vimeo = driver.find_elements(By.CSS_SELECTOR, "#column_left > div:n
 
 print([
     "Studio Title: ", studio_title[0].text,
+    "/n",
     "Country: ", studio_country[0].text,
     "City: ", studio_city[0].text,
-    "Website: ", website_link[0].text,
-    "Email: ", email[0].text, 
+    # "Website: ", website_link[0].text,
+    # "Email: ", email[0].text, 
     "Behance: ", socail_media_behance[0].text,
     "Facebook: ", socail_media_facebook[0].text,
     "Instagram: ", socail_media_instagram[0].text,
     "Youtube: ", socail_media_youtube[0].text,
     "Vimeo: ", socail_media_vimeo[0].text,
     # "Related Project Number: ", related_project_number
-
     ])	
 
+for website in websites:
+    herf_website = website.get_attribute('href')
+    if herf_website is not None:
+        print("website: ", herf_website)
+    else: 
+        print("No link found !!!")
+
+for email in emails:
+    herf_email = email.get_attribute('herf')
+    if herf_email is not None:
+        print("email: ", herf_email)
+    else:
+        print("no email Found !!!")
+
 driver.quit()
-
-
-
-
-
-
-
-
-
-
-
 
 
 
